ebf27ca9706eaedf88e08dca4279c0a14ce3a29b
Eric Lam <eric.lam42@hotmail.com>
2024-11-14 09:58:53 -0500

    Added api routes
    - historical data
    - news

--- client/src/app/api/fetchStockInfo.ts
+++ client/src/app/api/fetchStockInfo.ts
@@ -9,3 +9,45 @@ export const getTickerInfo = async (ticker: string) => {
     throw new Error("Failed to fetch data.");
   }
 };
+
+export const getTickerInfoBulk = async (tickers: string[]) => {
+  try {
+    const tickersString = tickers.toString();
+    const res = await fetch(
+      `https://h5o5bfmm0c.execute-api.us-east-2.amazonaws.com/dev/get-list-of-companies?tickers=${tickersString}`
+    );
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to fetch data.");
+  }
+};
+
+export const getTickerHistoricalData = async (
+  ticker: string,
+  period: string,
+  interval: string
+) => {
+  // See possible periods and intervals in ./app/lib/constants
+  try {
+    const res = await fetch(
+      `https://h5o5bfmm0c.execute-api.us-east-2.amazonaws.com/dev/get-stock-time-series?ticker=${ticker}&period=${period}&interval=${interval}`
+    );
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to fetch historical data.");
+  }
+};
+
+export const getTickerNews = async (ticker: string) => {
+  try {
+    const res = await fetch(
+      `https://h5o5bfmm0c.execute-api.us-east-2.amazonaws.com/dev/get-stock-news?ticker=${ticker}`
+    );
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to fetch stock news data.");
+  }
+};
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,60 +2,61 @@
 
 import React, { useState, useEffect } from "react";
 import { Typography } from "@mui/material";
-import { getTickerInfo } from "./api/fetchStockInfo";
 import Table from "./components/table";
 import { redirect } from "next/navigation";
+import sp500Data from "../../public/sp500Data.json";
+import { getTickerInfoBulk } from "./api/fetchStockInfo";
 
-const tickers = [
-  "nke",
-  "eric",
-  "msft",
-  "goog",
-  "aapl",
-  "spot",
-  "meta",
-  "amzn",
-  "tsla",
-  "nvda",
-];
+const tickers = sp500Data.map((ele) => ele.symbol).slice(0, 25);
 
 function Home() {
+  // Temporarily redirect users to signup page on production
   if (process.env.NODE_ENV !== "development") {
     redirect("/signup");
   }
 
-  //let data = await getTickerInfo("nke");
-  //console.log(data["address1"]);
-  // Fetch data for all tickers on the server-side
   // State to hold the fetched data
   const [data, setData] = useState([]);
+
+  // Number of tickers to fetch at once
+  const batchSize = 10;
+  const dataPromises: any[] = [];
 
   useEffect(() => {
     // Function to fetch data for all tickers
     const fetchData = async () => {
-      const dataPromises = tickers.map(async (ticker) => {
+      for (let i = 0; i < tickers.length; i += batchSize) {
+        const batch = tickers.slice(i, i + batchSize);
         try {
-          const data = await getTickerInfo(ticker);
-          return data;
-        } catch (err) {
-          console.error(`Error fetching data for ${ticker}`, err);
-          return null;
+          const info = await getTickerInfoBulk(batch);
+          dataPromises.push(...info);
+        } catch (error) {
+          console.error(`Error fetching data for ${batch}`, error);
         }
-      });
+      }
+
+      // const dataPromises = tickers.map(async (ticker) => {
+      //   try {
+      //     const data = await getTickerInfo(ticker);
+      //     return data;
+      //   } catch (err) {
+      //     console.error(`Error fetching data for ${ticker}`, err);
+      //     return null;
+      //   }
+      // });
 
       // Wait for all promises to resolve
       const resolvedData = await Promise.all(dataPromises);
       // Filter out any null values
       const filteredData = resolvedData.filter((item) => item !== null);
       // Update state with the filtered data
-      // console.log(filteredData);
       // @ts-expect-error
       setData(filteredData);
     };
 
     // Call the fetchData function
     fetchData();
-  }, []); // Empty dependency array to run only once on component mount
+  }, [tickers]); // Empty dependency array to run only once on component mount
 
   return (
     <main className="flex flex-col h-screen">
